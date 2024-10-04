27584fe9a20b4016861fd8753d48bf0b1164470a
José Wolf <wolf.jose89@gmail.com>
2024-10-03 23:59:23 -0300

    Resolvendo exercício 1014 da beecrowd sobre consumo.

--- src/com/br/Beecrowd/java/easy/bee1014/Consumo.java
+++ src/com/br/Beecrowd/java/easy/bee1014/Consumo.java
@@ -1,2 +1,23 @@
-package com.br.Beecrowd.java.easy.bee1014;public class Consumo {
+package com.br.Beecrowd.java.easy.bee1014;
+
+import java.util.Scanner;
+
+public class Consumo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double distancia = Double.parseDouble(sc.next());
+        double combustivelGasto =  Double.parseDouble(sc.next());
+
+        double percursso = distancia / combustivelGasto;
+
+        System.out.printf("%.3f km/l\n",percursso);
+
+        sc.close();
+
+
+    }
+
 }
