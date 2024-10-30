a910df40dc1a1e050e14aba1754294d18a5fcad2
José Wolf <wolf.jose89@gmail.com>
2024-10-29 23:20:00 -0300

    Resolvendo exercício bee1016 da beecrowd.

---
+++ src/com/br/Beecrowd/java/easy/bee1016/Distância.java
@@ -0,0 +1,18 @@
+package com.br.Beecrowd.java.easy.bee1016;
+
+import java.util.Scanner;
+
+public class Distância {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int km = sc.nextInt();
+        System.out.println((km*2) + " minutos");
+
+
+        sc.close();
+
+
+    }
+
+}
