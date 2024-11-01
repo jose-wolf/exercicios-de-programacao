a9d2175d49eb8131626459aa50f32415d1501a8b
José Wolf <wolf.jose89@gmail.com>
2024-11-01 20:12:08 -0300

    Resolvendo exercício Returning String do codewars.

---
+++ src/com/br/codewars/java/eightkyu/returningstrings/ReturningStrings.java
@@ -0,0 +1,12 @@
+package com.br.codewars.java.eightkyu.returningstrings;
+
+public class ReturningStrings {
+
+    public static void main(String[] args) {
+        System.out.println(greet("Ryan"));
+    }
+    public static String greet(String name){
+        return "Hello, " + name + " how are you doing today?";
+    }
+
+}
