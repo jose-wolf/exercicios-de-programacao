bb01a889f0c79ac89b3d5a78e94b769ca3d5bf59
José Wolf <wolf.jose89@gmail.com>
2024-11-01 18:21:10 -0300

    Resolvendo exercício Convert a Number to a String da codewars.

---
+++ src/com/br/codewars/java/eightkyu/convertanumbertoastring/Teste.java
@@ -0,0 +1,26 @@
+package com.br.codewars.java.eightkyu.convertanumbertoastring;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        int numero = 67;
+        int numero2 = 123;
+        int numero3 = 999;
+
+        if (numberToString(numero).equals("67"))
+            System.out.println("Convertido");
+
+        if ("123".equals(numberToString(123)))
+            System.out.println("Convertido");
+
+        if ("999".equals(numberToString(999)))
+            System.out.println("Convertido");
+
+    }
+
+    public static String numberToString(int num){
+        return Integer.toString(num);
+    }
+
+
+}
