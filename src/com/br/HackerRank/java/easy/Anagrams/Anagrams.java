a296c60fe4ad931168907256ca71fd5c8016e5fd
José Wolf <wolf.jose89@gmail.com>
2024-11-05 22:54:55 -0300

    Resolvendo exercicio de Java Anagrams do HackerRank.

---
+++ src/com/br/HackerRank/java/easy/Anagrams/Anagrams.java
@@ -0,0 +1,34 @@
+package com.br.HackerRank.java.easy.Anagrams;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagrams {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        boolean ret = isAnagram(a,b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams");
+
+        sc.close();
+    }
+
+
+    public static boolean isAnagram(String a, String b){
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        char[] aArray = a.toCharArray();
+        char[] bArray = b.toCharArray();
+        Arrays.sort(aArray);
+        Arrays.sort(bArray);
+        String aSorted = new String(aArray);
+        String bSorted = new String(bArray);
+
+        return aSorted.equals(bSorted);
+    }
+}
