20d4d76c3edbecb5464935008f7901d242a0b299
José Wolf <wolf.jose89@gmail.com>
2024-11-07 18:16:36 -0300

    Adicionando método que extrai o nome de uma String e a retorna, e resolvendo exercício da Codewars.

---
+++ src/com/br/codewars/java/eightkyu/grasshopper/GrasshopperDebugSayHello.java
@@ -0,0 +1,14 @@
+package com.br.codewars.java.eightkyu.grasshopper;
+
+public class GrasshopperDebugSayHello {
+
+    public static void main(String[] args) {
+        String name = "Mr. Spock";
+        System.out.println(sayHello(name));
+    }
+
+    public static String sayHello(String name){
+        return "Hello, " + name;
+    }
+
+}
