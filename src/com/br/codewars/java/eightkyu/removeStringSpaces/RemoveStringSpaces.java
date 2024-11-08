1ada7b082d23e0f70f60fab608cb7ac76e0e3cf1
José Wolf <wolf.jose89@gmail.com>
2024-11-08 10:43:31 -0300

    Adicionar método que remove os espaços e agrupa os caracteres.

--- src/com/br/codewars/java/eightkyu/removeStringSpaces/RemoveStringSpaces.java
+++ src/com/br/codewars/java/eightkyu/removeStringSpaces/RemoveStringSpaces.java
@@ -1,2 +1,14 @@
-package com.br.codewars.java.eightkyu.removeStringSpaces;public class RemoveStringSpaces {
+package com.br.codewars.java.eightkyu.removeStringSpaces;
+
+public class RemoveStringSpaces {
+
+    public static void main(String[] args) {
+        System.out.println(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+    }
+
+
+    public static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+
 }
