63c06a6c2eb0658826e067ddc3dcff72fadab910
José Wolf <wolf.jose89@gmail.com>
2024-11-12 14:56:11 -0300

    Adicionar método que retorna o tamenho e soma de um array, ignora zeros na contagem e soma.

--- src/com/br/codewars/java/eightkyu/countOfPositivesSumOfNegatives/Kata.java
+++ src/com/br/codewars/java/eightkyu/countOfPositivesSumOfNegatives/Kata.java
@@ -10,35 +10,47 @@ public class Kata {
         int[] input = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15};
         int[] input2 = new int[] {0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14};
 
-        System.out.println(Arrays.toString(soamrNegativos(input)));
-        System.out.println(Arrays.toString(soamrNegativos(input2)));
-        System.out.println(Arrays.toString(contarPositivos(input)));
-        System.out.println(Arrays.toString(contarPositivos(input2)));
+        System.out.println(Arrays.toString(countPositivesSumNegatives(input)));
+        System.out.println(Arrays.toString(countPositivesSumNegatives(input2)));
+
 
     }
 
-    public static int[] contarPositivos(int[] input){
+    public static int[] countPositivesSumNegatives(int[] input) {
+        List<Integer> lista = new ArrayList<>(input.length);
+        int[] resultado = new int[2];
+
+        for(int elemento : input){
+            lista.add(elemento);
+            if(elemento == 0){
+                lista.remove(Integer.valueOf(elemento));
+            }
+        }
+        resultado[0] = contarPositivos(input);
+        resultado[1] = soamrNegativos(input);
+
+        return resultado;
+    }
+
+    public static int contarPositivos(int[] input){
         int positivo = 0;
         for(int elemento : input){
             if (elemento > 0){
                 positivo++;
             }
         }
-        int[] resultado = new int[1];
-        resultado[0] = positivo;
-        return resultado;
+
+        return positivo;
     }
 
 
-    public static int[] soamrNegativos(int[] input){
+    public static int soamrNegativos(int[] input){
         int soma = 0;
         for(int elemento : input){
             if(elemento < 0){
                 soma += elemento;
             }
         }
-        int[] resultado = new int[1];
-        resultado[0] = soma;
-        return resultado;
+        return soma;
     }
 }
