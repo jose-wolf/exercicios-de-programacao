427c1689eaaa457255c37106a1fae109f0b1484e
José Wolf <wolf.jose89@gmail.com>
2024-11-18 19:50:44 -0300

    Corrigir: corrigindo métodos que retornavam um array, para que agora retornassem a soma de x1 + x2.

--- src/com/br/codewars/java/sevenkyu/findTheSumOfTheRootsOfAQuadraticEquation/Solution.java
+++ src/com/br/codewars/java/sevenkyu/findTheSumOfTheRootsOfAQuadraticEquation/Solution.java
@@ -6,8 +6,9 @@ import java.util.OptionalDouble;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(oneEquationType(3,-27)));
-        System.out.println(Arrays.toString(secondEquationType(5,-45)));
+        System.out.println((oneEquationType(6,-24)));
+        System.out.println((secondEquationType(-5,21)));
+        System.out.println((bhaskara(1,-35,-23)));
     }
 
     public static OptionalDouble roots(double a, double  b, double c){
@@ -15,28 +16,31 @@ public class Solution {
         return OptionalDouble.empty();
     }
 
-    public static double[] oneEquationType(double a, double c){
+    public static double oneEquationType(double a, double c){
         double resultadoDaEquacao = -c / a;
         double x1 = 0.0;
         double x2 = 0.0;
         if(resultadoDaEquacao < 0){
-            return new double[] {0};
+            return 0;
         }else{
             x1 = Math.sqrt(resultadoDaEquacao);
             x2 = -x1;
-            return new double[] {x1,x2};
+
+            return x1 + x2;
         }
     }
 
-    public static double[] secondEquationType(double a, double b){
+    public static double secondEquationType(double a, double b){
         double resultadoDaEquacao = -b / a;
         double x1 = 0.0;
         double x2 = 0.0;
         if(a == 0){
-            return new double[] {0};
+            return 0;
         }else{
             x2 = -b / a;
-            return new double[] {x1,x2};
+            return x1 + x2;
         }
     }
+
+    
 }
