c065fa596025d39117adfd592496279dc4ddf0a3
José Wolf <wolf.jose89@gmail.com>
2024-11-18 17:30:38 -0300

    Adicionar: método que calcula a equação do tipo ax² + c = 0.

--- src/com/br/codewars/java/sevenkyu/findTheSumOfTheRootsOfAQuadraticEquation/Solution.java
+++ src/com/br/codewars/java/sevenkyu/findTheSumOfTheRootsOfAQuadraticEquation/Solution.java
@@ -1,2 +1,31 @@
-package com.br.codewars.java.sevenkyu.findTheSumOfTheRootsOfAQuadraticEquation;public class Solution {
+package com.br.codewars.java.sevenkyu.findTheSumOfTheRootsOfAQuadraticEquation;
+
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(oneEquationType(3,-27)));
+    }
+
+    public static OptionalDouble roots(double a, double  b, double c){
+
+        return OptionalDouble.empty();
+    }
+
+    public static double[] oneEquationType(double a, double c){
+        double resultadoDaEquacao = -c / a;
+        double x1 = 0.0;
+        double x2 = 0.0;
+        if(resultadoDaEquacao < 0){
+            return new double[] {0};
+        }else{
+            x1 = Math.sqrt(resultadoDaEquacao);
+            x2 = -x1;
+            return new double[] {x1,x2};
+        }
+    }
+
+
 }
