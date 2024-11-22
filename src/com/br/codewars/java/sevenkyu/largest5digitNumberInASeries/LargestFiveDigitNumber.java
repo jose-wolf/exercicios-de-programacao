8cd99bb88265cf61ca632fd689b1238896aebada
José Wolf <wolf.jose89@gmail.com>
2024-11-22 19:32:51 -0300

    Adicionar: método que recebe número string e retotrna o maior valor de acordo com o número de dígitos.

---
+++ src/com/br/codewars/java/sevenkyu/largest5digitNumberInASeries/LargestFiveDigitNumber.java
@@ -0,0 +1,24 @@
+package com.br.codewars.java.sevenkyu.largest5digitNumberInASeries;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LargestFiveDigitNumber {
+
+    public static void main(String[] args) {
+        System.out.println(solve("2839103"));
+        System.out.println(solve("1234567890"));
+        System.out.println(solve("731674765"));
+    }
+
+    public static int solve(final String digits) {
+        List<Integer> numeros = new ArrayList<>();
+        int digitosConsecutivos = 5;
+        for(int i = 0; i <= digits.length() - digitosConsecutivos; i++){
+            String sequencia = digits.substring(i,i + digitosConsecutivos);
+            numeros.add(Integer.parseInt(sequencia));
+        }
+        return Collections.max(numeros); // you code here
+    }
+}
